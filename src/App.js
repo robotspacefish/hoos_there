6fc15ad8e363282dd721f8a252e5638b55d3a563
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-24 11:58:02 -0400

    app skeleton & import json data & set to default props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import creaturesData from './assets/creatures.json';
+import React, { Component } from 'react';
+import CreaturesContainer from './Creatures/CreaturesContainer';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const json = require('./assets/creatures.json');
 
-    </div>
-  );
+class App extends Component {
+  static defaultProps = {
+    creatures: JSON.parse(JSON.stringify(json))
+  };
+
+  state = {
+    currentCreatures: []
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* header */}
+        <CreaturesContainer currentCreatures={this.state.currentCreatures} />
+        {/* footer */}
+      </div>
+    );
+  }
 }
 
 export default App;

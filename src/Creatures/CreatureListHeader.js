7e36981b7f370f9ae5dc8d05f97396fce9a8f18a
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-25 17:06:20 -0400

    creature list header skeleton

---
+++ src/Creatures/CreatureListHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreatureListHeader = props => (
+  <div className="CreatureListHeader">
+    TODO: TypeButtons <br />
+    TODO: SortButtons <br />
+    TODO: HemisphereButtons <br />
+  </div>
+);
+
+export default CreatureListHeader;

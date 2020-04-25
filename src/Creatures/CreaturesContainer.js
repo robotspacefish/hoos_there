b743c0c364832cfd0ec68dd59b20b52b60e54077
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-25 17:13:13 -0400

    pass setType

--- src/Creatures/CreaturesContainer.js
+++ src/Creatures/CreaturesContainer.js
@@ -10,7 +10,7 @@ export default class CreaturesContainer extends Component {
   render() {
     return (
       <>
-        <CreatureListHeader />
+        <CreatureListHeader setType={this.props.setType} />
         <CreatureList creatures={this.props.currentCreatures} />
       </>
     )

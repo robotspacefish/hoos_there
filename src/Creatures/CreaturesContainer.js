11974683aebcc796f7799e3b3d1580a611e79be5
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-25 14:41:19 -0400

    bug and fish filters

--- src/Creatures/CreaturesContainer.js
+++ src/Creatures/CreaturesContainer.js
@@ -3,6 +3,9 @@ import CreatureListHeader from './CreatureListHeader';
 import CreatureList from './CreatureList';
 
 export default class CreaturesContainer extends Component {
+  bugs = creatures => (creatures.filter(c => c.type === "bug"));
+
+  fish = creatures => (creatures.filter(c => c.type === "fish"));
 
   render() {
     return (

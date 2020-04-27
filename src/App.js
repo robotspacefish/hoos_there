254b20a0aa7932fe553e68aceea455e43a8755d9
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-27 15:56:39 -0400

    move creature filter and sorting functions to a separate file

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import CreaturesContainer from './Creatures/CreaturesContainer';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import Container from 'react-bootstrap/Container';
-import { sortAlpha, sortNumeric } from './helpers/helpers';
+// import { sortAlpha, sortNumeric } from './helpers/helpers';
 import { connect } from 'react-redux';
 import { setCurrentTime } from './actions/clockActions';
 import { getCurrentlyAvailableCreatures } from './actions/creatureActions';
+import { filterByDisplayTypeAndSort } from './helpers/sortAndFilterCreatures';
 
 import './App.css';
 
@@ -21,64 +22,6 @@ class App extends Component {
   state = {
     sort: { type: 'default', direction: 'default', icon: '' }
   }
-
-  sortCreatures = (creatures) => {
-    let sortedCreatures;
-    switch (this.state.sort.type) {
-      case 'name':
-      case 'type':
-      case 'location':
-        sortedCreatures = sortAlpha(creatures, this.state.sort.type)
-        break;
-      case "shadow":
-        sortedCreatures = this.sortByShadowSize(creatures);
-        break;
-      case "time":
-        sortedCreatures = this.sortByAvailableTime(creatures);
-        break;
-      case "price":
-        sortedCreatures = sortNumeric(creatures, "price")
-        break;
-      default:
-    }
-
-    // // dsc sort
-    if (this.state.sort.direction === 'dsc') sortedCreatures = sortedCreatures.reverse()
-
-    return sortedCreatures;
-
-  };
-
-  sortByShadowSize(creatures) {
-    // some creatures don't have a shadow, some have a 'narrow' shadow
-    // I separated them from number size shadows, converted those to integers,
-    // sorted, and joined and flatted those 2 arrays in 'asc' order
-    const type = 'shadow_size'
-    const narrowOrNACreatures = creatures.filter(c => c.shadow_size === "Narrow" || c.shadow_size === "NA");
-    const nonNarrowCreatures = creatures.filter(c => c.shadow_size !== "Narrow" && c.shadow_size !== "NA").map(c => (
-      { ...c, shadow_size: parseInt(c.shadow_size) }
-    ));
-    const sortedNarrowOrNaCreatures = sortAlpha(narrowOrNACreatures, type);
-    const sortedNonNarrowCreatures = sortNumeric(nonNarrowCreatures, type);
-
-    return [...sortedNarrowOrNaCreatures, sortedNonNarrowCreatures].flat();
-  }
-
-  sortByAvailableTime = creatures => (
-    [...creatures].sort((a, b) => {
-      const creatureA = this.getCreaturesFirstTimeAvailable(a);
-      const creatureB = this.getCreaturesFirstTimeAvailable(b);
-
-      return creatureA.start_time - creatureB.start_time
-    })
-  );
-
-  getCreaturesFirstTimeAvailable = (creature) => (
-    // if creature has 1 available time, return that, otherwise sort the times
-    // and return the first one
-    creature.available_times.length === 1 ?
-      { ...creature.available_times[0] } : [...creature.available_times].sort((availA, availB) => availA.start_time - availB.start_time)[0]
-  );
 
   updateType = (type, value) => (this.setState({ [type]: value }));
 
@@ -121,21 +64,8 @@ class App extends Component {
     }
   }
 
-  filterByDisplayType = () => {
-    const { displayType, currentCreatures } = this.props;
-    return displayType === 'all' ?
-      currentCreatures :
-      currentCreatures.filter(creature => creature.type === displayType);
-  }
-
-  filterByDisplayTypeAndSort = () => {
-    const creatures = this.filterByDisplayType();
-    return this.state.sort.type === 'default' ?
-      creatures : this.sortCreatures(creatures);
-  }
-
   render() {
-    const creatures = this.filterByDisplayTypeAndSort();
+    const creatures = filterByDisplayTypeAndSort(this.state.sort, this.props.displayType, this.props.currentCreatures);
     return (
       <Container>
         < Header now={this.props.now.format("dddd, MMMM Do YYYY, h:mm A")} updateCurrentTime={this.props.setCurrentTime} />
@@ -165,7 +95,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => {
   return {
     setCurrentTime: () => dispatch(setCurrentTime()),
-    getCurrentlyAvailableCreatures: (creatures, months, hemisphere, now) => dispatch(getCurrentlyAvailableCreatures(creatures, months, hemisphere, now)),
+    getCurrentlyAvailableCreatures: (creatures, months, hemisphere, now) => dispatch(getCurrentlyAvailableCreatures(creatures, months, hemisphere, now))
   }
 };
 
---
+++ src/helpers/sortAndFilterCreatures.js
@@ -0,0 +1,71 @@
+import { sortAlpha, sortNumeric } from './helpers';
+
+export const filterByDisplayTypeAndSort = (sort, displayType, currentCreatures) => {
+  const creatures = filterByDisplayType(displayType, currentCreatures);
+  return sort.type === 'default' ?
+    creatures : sortCreatures(sort, creatures);
+}
+
+const filterByDisplayType = (displayType, currentCreatures) => {
+  return displayType === 'all' ?
+    currentCreatures :
+    currentCreatures.filter(creature => creature.type === displayType);
+}
+
+const sortCreatures = (sort, creatures) => {
+  let sortedCreatures;
+  switch (sort.type) {
+    case 'name':
+    case 'type':
+    case 'location':
+      sortedCreatures = sortAlpha(creatures, sort.type)
+      break;
+    case "shadow":
+      sortedCreatures = sortByShadowSize(creatures);
+      break;
+    case "time":
+      sortedCreatures = sortByAvailableTime(creatures);
+      break;
+    case "price":
+      sortedCreatures = sortNumeric(creatures, "price")
+      break;
+    default:
+  }
+
+  // // dsc sort
+  if (sort.direction === 'dsc') sortedCreatures = sortedCreatures.reverse()
+
+  return sortedCreatures;
+
+};
+
+const sortByShadowSize = (creatures) => {
+  // some creatures don't have a shadow, some have a 'narrow' shadow
+  // I separated them from number size shadows, converted those to integers,
+  // sorted, and joined and flatted those 2 arrays in 'asc' order
+  const type = 'shadow_size'
+  const narrowOrNACreatures = creatures.filter(c => c.shadow_size === "Narrow" || c.shadow_size === "NA");
+  const nonNarrowCreatures = creatures.filter(c => c.shadow_size !== "Narrow" && c.shadow_size !== "NA").map(c => (
+    { ...c, shadow_size: parseInt(c.shadow_size) }
+  ));
+  const sortedNarrowOrNaCreatures = sortAlpha(narrowOrNACreatures, type);
+  const sortedNonNarrowCreatures = sortNumeric(nonNarrowCreatures, type);
+
+  return [...sortedNarrowOrNaCreatures, sortedNonNarrowCreatures].flat();
+}
+
+const sortByAvailableTime = creatures => (
+  [...creatures].sort((a, b) => {
+    const creatureA = getCreaturesFirstTimeAvailable(a);
+    const creatureB = getCreaturesFirstTimeAvailable(b);
+
+    return creatureA.start_time - creatureB.start_time
+  })
+);
+
+const getCreaturesFirstTimeAvailable = creature => (
+  // if creature has 1 available time, return that, otherwise sort the times
+  // and return the first one
+  creature.available_times.length === 1 ?
+    { ...creature.available_times[0] } : [...creature.available_times].sort((availA, availB) => availA.start_time - availB.start_time)[0]
+);

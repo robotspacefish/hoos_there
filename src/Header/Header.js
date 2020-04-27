a3968d1ad637b07926df5944d138ebcf654db70b
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-27 15:31:06 -0400

    delete logo container div

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,9 +5,7 @@ import owl from '../assets/images/owl.png';
 const Header = props => (
   <header>
     <div className="Header__title-container">
-      <div>
-        <img className="logo" src={owl} alt="Hoo's There logo" />
-      </div>
+      <img className="logo" src={owl} alt="Hoo's There logo" />
       <h1 className="title">Hoo's There?</h1>
     </div>
     <Clock now={props.now} updateCurrentTime={props.updateCurrentTime} />

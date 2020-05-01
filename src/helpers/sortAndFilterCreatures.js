79bbc332cc4a290cda894e6ddec725324db3ccdb
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-05-01 12:03:55 -0400

    add search querying to filters

--- src/helpers/sortAndFilterCreatures.js
+++ src/helpers/sortAndFilterCreatures.js
@@ -2,9 +2,21 @@ import { sortAlpha, sortNumeric } from './helpers';
 
 export const allFilters = (sort, displayType, currentCreatures, query) => {
   const creatures = filterByDisplayType(displayType, currentCreatures);
+  const queriedCreatures = query === '' ? creatures : queryCreatures(creatures, query);
+  /** TODO handle no results */
   return sort.type === 'default' ?
-    creatures : sortCreatures(sort, creatures);
+    queriedCreatures : sortCreatures(sort, queriedCreatures);
 }
+
+const queryCreatures = (creatures, q) => {
+  /** Search by name, location, or price */
+  const query = q.toLowerCase();
+  return creatures.filter(creature => (
+    creature.name.toLowerCase().includes(query) ||
+    creature.location.toLowerCase().includes(query) ||
+    creature.price.includes(query)
+  ));
+};
 
 const filterByDisplayType = (displayType, currentCreatures) => {
   return displayType === 'all' ?

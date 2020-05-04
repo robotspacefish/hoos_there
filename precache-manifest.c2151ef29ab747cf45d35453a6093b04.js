f575a036253d5fbeaf403f04afb83f672ce7d90d
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-05-04 12:23:52 -0400

    Updates

---
+++ precache-manifest.c2151ef29ab747cf45d35453a6093b04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cafd3087e601f78a68df5ab0657423a",
+    "url": "/hoos_there/index.html"
+  },
+  {
+    "revision": "32e8db7f31378cd00d56",
+    "url": "/hoos_there/static/css/main.401550f3.chunk.css"
+  },
+  {
+    "revision": "6aa318f27ada740fbe2a",
+    "url": "/hoos_there/static/js/2.9772c9a9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hoos_there/static/js/2.9772c9a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e8db7f31378cd00d56",
+    "url": "/hoos_there/static/js/main.e2776efb.chunk.js"
+  },
+  {
+    "revision": "70fa2dd680a8c9d1c858",
+    "url": "/hoos_there/static/js/runtime-main.91f1658a.js"
+  },
+  {
+    "revision": "796b643a146db4b34af5f263f2b19e4a",
+    "url": "/hoos_there/static/media/FinkHeavy.796b643a.ttf"
+  }
+]);
--- src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import creatures from './creaturesReducer';
-import clock from './clockReducer';
-
-export default combineReducers({
-  creatures,
-  clock
-});

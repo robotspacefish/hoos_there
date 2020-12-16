4504257f09cdf86fe51ca1242078f5f84a7620bf
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-16 11:38:13 -0500

    fix: add October to months array

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { setCurrentTime } from './actions/clockActions';
 import './App.css';
 
 function App(props) {
-  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"];
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
   return (
     <Container>

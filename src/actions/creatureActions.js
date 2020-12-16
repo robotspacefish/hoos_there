43b39e8c6674dbacffacae924b16f27ce04c300f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-16 12:03:29 -0500

    refactor isOutAtThisTime without need for time pkg

--- src/actions/creatureActions.js
+++ src/actions/creatureActions.js
@@ -15,23 +15,21 @@ const isOutInThisMonth = (creature, months, hemisphere, now) => {
 }
 
 const isOutAtThisTime = (availableTimes, now) => {
-  const startTime = availableTimes.start_time,
-    endTime = availableTimes.end_time;
-
   if (availableTimes.time === "All day") return true;
 
-  let s = moment();
-  let e = moment();
-  s.hour(startTime)
-  e.hour(endTime)
+  const startHour = availableTimes.start_time,
+    endHour = availableTimes.end_time,
+    nowDay = now.day(),
+    nowHour = now.hour(),
+    endDay = endHour < startHour ? now.day() + 1 : now.day();
 
-  if (endTime < startTime) e.day(e.day() + 1)
-
-  // if current time is between start and end times or
-  // if current time is between start time and end time is the next day
-  return (s.hour() <= now.hour() && e.hour() > now.hour()) ||
-    (now.hour() < e.hour() && e.hour() < s.hour() && e.day() > now.day()) ||
-    (s.hour() <= now.hour() && e.day() > now.day());
+  /**
+   * if current time is between start and end times or
+   * if current time is between start time and end time is the next day
+   */
+  return (startHour <= nowHour && endHour > nowHour) ||
+    (nowHour < endHour && endHour < startHour && endDay > nowDay) ||
+    (startHour <= nowHour && endDay > nowDay);
 }
 
 export const updateSort = (currentSort, type) => {
